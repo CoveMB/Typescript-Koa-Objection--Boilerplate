d8b0b250b80598f13593f129b6dba139dfd17750
Benjamin <bmarquiscom@gmail.com>
2020-07-12 06:50:55 -0400

    Refactor request binded types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,9 +1,5 @@
-import { Middleware } from 'koa';
-
 export interface Constructable<M> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   // We want this Constructable to be flexible so we will accept any here
   new(...opt: any[]): M;
 }
-
-type Controller = Middleware;
